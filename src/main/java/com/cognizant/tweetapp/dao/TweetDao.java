21988961dd8425b623dfa6258b7015a736da941b
Maruti@05 <40956108+Maruti05@users.noreply.github.com>
2022-11-27 18:50:52 +0530

    tweet-app

---
+++ src/main/java/com/cognizant/tweetapp/dao/TweetDao.java
@@ -0,0 +1,26 @@
+package com.cognizant.tweetapp.dao;
+
+import java.util.List;
+
+import com.cognizant.tweetapp.entity.Tweet;
+import com.cognizant.tweetapp.entity.User;
+
+public interface TweetDao {
+
+	boolean login(User user);
+
+	String register(User user);
+
+	String forgotPassword(String email);
+
+	boolean logout(User user);
+
+	boolean resetPassword(User user, String newPassword);
+
+	boolean postTweet(String message, User user);
+
+	List<Tweet> viewAllTweets(User loggedUser);
+
+	List<User> viewAllTweets();
+
+}
